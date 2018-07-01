a51939bff15169b17be60933e481f371248ac9b0
Ankur Jaiswar <jaiswar.ankur@gmail.com>
2018-07-01 17:29:54 +0530

    Initial commit

---
+++ app/src/main/java/com/festapp/CreateEvent.kt
@@ -0,0 +1,12 @@
+package com.festapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateEvent : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_event)
+    }
+}
